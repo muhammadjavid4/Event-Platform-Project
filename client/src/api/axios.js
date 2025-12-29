c2a9a37245366ef0a50e52087e477426353d268e
Muhammad Javid Pasha <muhammadjavid622@gmail.com>
2025-12-29 16:58:28 +0530

    Update backend URL for production

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://event-platform-backend-jw7y.onrender.com/api"
 });
 
 api.interceptors.request.use((req) => {
--- server/app.js
+++ server/app.js
@@ -7,7 +7,9 @@ const rsvpRoutes = require("./routes/rsvp.routes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
